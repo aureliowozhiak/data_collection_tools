12e7ad0587235d080fa670173ced1c1ebce73220
Marcos Aurelio Wozhiak Junior <aureliowozhiak@gmail.com>
2022-11-18 10:56:56 -0300

    <FEATURE>Download table CSV

--- src/controller/extract/scraper.py
+++ src/controller/extract/scraper.py
@@ -6,6 +6,8 @@ sys.path.append('../')
 
 from src.model.extract.scraper import modelScraper
 
+sys.path.append('cherrypy/')
+tabletools_js = open('src/js/tabletools.js', 'r').read()
 class controllerScraper:
     
 
@@ -13,14 +15,17 @@ class controllerScraper:
         return None
 
     def main(input_value=None, scraper_type=None, index=0):
-
+        
+        
         scraper_type = str(scraper_type)
         input_value = str(input_value)
         index = int(index)
 
         if scraper_type == "table_scraping":
             try:
-                return f"{modelScraper.get_tables(f'{input_value}')[index].to_html()}"
+                return f'<script>{tabletools_js}</script> \
+                    {modelScraper.get_tables(f"{input_value}")[index].to_html()} <br> \
+                    <a href="#" onclick="download_table_as_csv(\'dataframe\');">Download as CSV</a>'
             except:
                 return f'{input_value}'
         else:
