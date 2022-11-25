fbff75a3b3170c4d1efbea6c85455ddc8e1e1ad2
Marcos Aurelio Wozhiak Junior <aureliowozhiak@gmail.com>
2022-11-25 18:54:19 -0300

    <FIX>Handling invalid expressions

--- src/view/web/another_tools/math.py
+++ src/view/web/another_tools/math.py
@@ -10,4 +10,15 @@ class viewMath:
         return None
 
     def calc_expression(expression):
-        return f'<strong>{expression}</strong>'
+        sintaxe = "<br><br><h3>Sintaxe das expressões</h3>\
+                    <p>Soma: 1 <b>+</b> 1 = 2 <br> \
+                    Subtração: 2 <b>-</b> 1 = 1<br> \
+                    Multiplicação: 2 <b>*</b> 2 <br> \
+                    Divisão: 4 <b>/</b> 2 = 2<br> \
+                    Potenciação: 2 <b>^</b> 3 = 8 ou 2<b>**</b>3 = 8<br> \
+                    Raiz quadrada: <b>sqrt(</b>10<b>)</b> <br> \
+                    Resto de divisão: 3<b>%</b>2 <br></p>"
+
+        if expression != "Invalid Expression Error":
+            return f'<strong>{expression}</strong>' + sintaxe
+        return "<strong>Expressão <strong>aritmética</strong> inválida!</strong><br><br><em>Tente utilizar a sintaxe padrão:<em>" + sintaxe
