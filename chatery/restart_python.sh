bc9bf34a5589726d5c8a40bcd4e41c841f4bf7c9
Marcos Aurelio Wozhiak Junior <aureliowozhiak@gmail.com>
2022-11-28 09:24:16 -0300

    <FIX>Reload on server restarting

--- chatery/restart_python.sh
+++ chatery/restart_python.sh
@@ -1,8 +1,14 @@
 while :
 do
   pkill python3
-  sleep 150
   echo "Restarting...."
+  sleep 50
+
+  echo "50...."
+  sleep 50
+
+  echo "100...."
+  sleep 50
 
   python3 delete_messages.py
   echo "Deleting messages...."
